d7399c6751ee81bd3da6d8404d3a3d5a8d8d29fd
hikobe8_outside <hikobe8@foxmail.com>
2015-12-22 17:32:48 +0800

    多用户留言系统 day13 work_day 博友页面 数字分页&文字分页

--- blog.php
+++ blog.php
@@ -28,15 +28,33 @@ require dirname ( __FILE__ ) . '/includes/common.inc.php';
 <body>
 <?php
 require ROOT_PATH . "includes/header.inc.php";
+/**
+ * $_page_num 页码
+ */
 $_page_num = 1;
+/**
+ * $_page_size 每页的数量
+ */
+$_page_size = 10;
+$_user_count = mysql_num_rows(_query("SELECT tg_id FROM tg_user WHERE tg_active=''"));
+/**
+ * $_page_count 总页数/最大页码
+ */
+$_page_count = ceil($_user_count / $_page_size);
 if(isset($_GET['page'])){
     $_page_num = $_GET['page'];
+    //检查GET传过来的非法页码
+    if(empty($_page_num) || !is_numeric($_page_num)) {
+        $_page_num = 1;
+    } else {
+        if($_page_num > $_page_count) {
+            $_page_num = $_page_count;
+        }
+        $_page_num = intval($_page_num);
+    }
+    
 }
-$_page_size = 10;
 $_start_index = ($_page_num - 1)*$_page_size;
-$_user_count = mysql_num_rows(_query("SELECT tg_id FROM tg_user WHERE tg_active=''"));
-//计算页数
-$_page_count = ceil($_user_count / $_page_size);
 $_sql = "SELECT tg_id,tg_username,tg_face,tg_sex FROM tg_user WHERE tg_active = '' ORDER BY tg_reg_time DESC LIMIT ".$_start_index.",".$_page_size;
 //取出所有激活的用户 并且以注册时间降序排序
 $_result = _query($_sql);
@@ -69,6 +87,28 @@ $_result = _query($_sql);
         echo '<li><a href="blog.php?page='.$_i.'">'.$_i.'</a></li>';      
     }
 ?>
+</ul>
+</div>
+<div id='page_text'>
+<ul> 
+    <li><?php echo $_page_num."/".$_page_count?>页 | </li>
+    <li>共有<?php echo $_user_count?>个会员 | </li>
+    <?php 
+        if ($_page_num == 1) {
+            echo "<li>首页 | </li>";
+            echo "<li>上一页 | </li>";
+        } else {
+            echo '<li><a href="'.SCRIPT.'.php">首页 </a>| </li>';
+            echo '<li><a href="'.SCRIPT.'.php?page='.($_page_num - 1).'">上一页</a> | </li>';
+        }
+        if ($_page_num == $_page_count) {
+            echo "<li>下一页 | </li>";
+            echo "<li>尾页</li>";
+        } else {
+            echo '<li><a href="'.SCRIPT.'.php?page='.($_page_num + 1).'">下一页 </a>| </li>';
+            echo '<li><a href="'.SCRIPT.'.php?page='.$_page_count.'">尾页</a></li>';
+        }
+    ?>
 </ul>
 </div>
 </div>
